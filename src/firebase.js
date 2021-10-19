ffa61d70e54b97934c56d035e737bc42b564bd04
YASH <yashkhatwani2002@gmail.com>
2021-10-19 18:44:24 +0530

    Package installation of firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,15 @@
 import firebase from "firebase";
 import "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDrLUA-ybhSmsvjo19wNyOMlFVmXvsEPA0",
+//   authDomain: "sportsworld-b6d53.firebaseapp.com",
+//   projectId: "sportsworld-b6d53",
+//   storageBucket: "sportsworld-b6d53.appspot.com",
+//   messagingSenderId: "378599160420",
+//   appId: "1:378599160420:web:208f1ff69306be416a3d71",
+//   measurementId: "G-HYLS4ZNGYL",
+// };
 const firebaseConfig = {
   apiKey: "AIzaSyA9GhajuVbmlDAz8J3BbV78BvT5g5guD7Y",
   authDomain: "clone-1a104.firebaseapp.com",
@@ -10,6 +19,7 @@ const firebaseConfig = {
   appId: "1:230941578010:web:bb778255f3c00347d792a3",
   measurementId: "G-HE59DK0RFZ",
 };
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
