b16fd3cbcc8a150ebfff6763d2dbfa19e925fbf2
Siddhant <siddhant.jk@gmail.com>
2021-10-19 12:59:11 +0530

    Contacts page and Brands Page added

---
+++ src/Brands/Brands.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Product from "../Product/Product";
+
+function Brands() {
+  return <div></div>;
+}
+
+export default Brands;
---
+++ src/Contacs/Contacts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Contacts() {
+  return (
+    <div>
+      <h1>I am the contacts Page !</h1>
+    </div>
+  );
+}
+
+export default Contacts;
